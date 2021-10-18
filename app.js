80dddfb3818460257bee040978d698914cd62e5b
Mubarak MC <mubbu.rock@gmail.com>
2021-10-18 15:40:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+// fetch("https://dog.ceo/api/breeds/list/all").then(function(response){
+//     return response.json()
+// }).then(function(data){
+//     console.log(data)
+// })
+
+
+async function start(){
+const response = await fetch("https://dog.ceo/api/breeds/list/all")
+const data = await response.json()
+createBreedList(data.message)
+}
+start()
+
+function createBreedList(breedList){
+    document.getElementById("breed").innerHTML=`
+    <select onchange="loadByBreed(this.value)">
+    <option>Choose a Dog Breed</option>
+   ${Object.keys(breedList).map(function(breed) {
+       return `<option>${breed}</option>`
+   }).join('')}
+</select>
+    `
+
+}
+async function loadByBreed(breed){
+if (breed != "Choose a Dog Breed"){
+  const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
+const data = await response.json()
+createSlideshow(data.message)
+}
+}
+
+function createSlideshow(images){
+    let currentPosition = 0
+ document.getElementById("slideshow").innerHTML = `
+ <div class="slide" style="background-image: url('${images[0]}')"></div>
+ <div class="slide" style="background-image: url('${images[1]}')"></div>
+
+ `
+ currentPosition += 2
+ setInterval(nextSlide, 3000)
+
+ function nextSlide(){
+     document.getElementById("slideshow").insertAdjacentHTML("beforeend",` <div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`)
+setTimeout(function(){
+    document.querySelector(".slide").remove()
+}, 1000)
+if (currentPosition + 1 >= images.length){
+    currentPosition = 0
+}else{
+    currentPosition++
+}
+ }
+
+}
